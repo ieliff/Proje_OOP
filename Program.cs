1d81528302a61fd03d65ad7b710789ca4b195ab2
ieliff <ielif6011@gmail.com>
2025-01-01 17:40:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Proje_OOP
+{
+    public enum OgretimDuzeyi
+    {
+        IlkOgretim,
+        OrtaOgretim,
+        Lise,
+        Lisans
+    }
+    internal class Program
+    {
+        public static void OgrenciNotHesapla(Ogrenci ogr)
+        {
+            ogr.NotHesapla();
+        }
+        static void Main(string[] args)
+        {
+            LiseOgrencisi liseOgr = new LiseOgrencisi("Fatma Yılmaz", 12345, 78, 45);
+
+            LisansOgrencisi lisansOgr = new LisansOgrencisi("Ayşe Kaya", 67890, "Mühendislik Fakültesi", 70, 80);
+
+            Console.WriteLine("\nLise Öğrencisi Not Hesaplama:");
+            OgrenciNotHesapla(liseOgr);
+
+            Console.WriteLine("\nLisans Öğrencisi Not Hesaplama:");
+            OgrenciNotHesapla(lisansOgr);
+
+        }
+    }
+}
